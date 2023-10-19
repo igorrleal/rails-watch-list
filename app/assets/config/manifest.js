a30ea9bc5a6f56896c179896c2295da9cb1c17b8
igor leal <goririo@gmail.com>
2023-10-19 20:49:41 -0300

    Prepare rails app with external specs

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,2 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link popper.js
-//= link bootstrap.min.js
---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,49 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @movies = @list.movies
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+
+    if @list.save
+      redirect_to @list, notice: 'List was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @list.update(list_params)
+      redirect_to @list, notice: 'List was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_url, notice: 'List was successfully destroyed.'
+  end
+
+  private
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+  def set_list
+    @list = List.find(params[:id])
+  end
+end
